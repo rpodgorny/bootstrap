780ebe2277821a0211b43b4e57df4908613a89ac
Radek Podgorny <radek@podgorny.cz>
2015-01-14 11:27:26 +0100

    added 4to6utils

---
+++ 4to6utils.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+set -x
+
+if [ -d /cygdrive/c/4to6utils ]; then
+	echo '4to6utils already installed?'
+	exit 1
+fi
+
+schtasks /delete /tn "4to6server" /f
+schtasks /delete /tn "ipv6listen" /f
+
+atxpkg install 4to6utils
+
+cd c:/4to6utils
+./install_service.bat
+cd -
+
+net start 4to6server
+
+echo "Po skonceni restartujte pocitac, bezi pravdepodobne dva procesy, ale nemuzeme ho vypnout, protoze bychom se odpojili. Nutno minulou instanci odinstalovat"
