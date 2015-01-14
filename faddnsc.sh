12940ed2d741e990f0765b8f3c0edf899623f874
Radek Podgorny <radek@podgorny.cz>
2015-01-14 11:28:18 +0100

    added faddnsc

---
+++ faddnsc.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+set -x
+
+if [ -d /cygdrive/c/faddnsc ]; then
+	echo 'faddnsc already installed?'
+	exit 1
+fi
+
+schtasks /delete /tn "nsupdate" /f
+schtasks /delete /tn "ns update" /f
+schtasks /delete /tn "faddns" /f
+schtasks /delete /tn "faddnsc" /f
+
+atxpkg install faddnsc
+
+cd c:/faddnsc
+sed -i 's/example.com/asterix.cz/g' faddnsc.ini
+nano -w faddnsc.ini
+./install_service.bat
+cd -
+
+net start faddnsc
+
+echo "Po skonceni restartujte pocitac, bezi pravdepodobne dva procesy. Nutno minulou instanci odinstalovat"
