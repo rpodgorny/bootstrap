53ae7046da65bead9ffffd21d07625c6486fef41
Radek Podgorny <radek@podgorny.cz>
2015-01-30 00:24:38 +0100

    cygwin_update: don't add desktop icon

--- cygwin_update.sh
+++ cygwin_update.sh
@@ -6,5 +6,4 @@ wget -c http://cygwin.com/setup-x86.exe
 
 #http://ftp.fsn.hu/pub/cygwin
 
-chmod a+x setup-x86.exe
-./setup-x86.exe -s http://mirror.switch.ch/ftp/mirror/cygwin -l c:/tmp/ -D -g -d -q
+./setup-x86.exe -s http://mirror.switch.ch/ftp/mirror/cygwin -l c:/tmp/ -q -g
