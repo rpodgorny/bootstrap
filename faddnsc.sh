a14b3cd12802edf98c1ad8f6d960927766c6128b
Radek Podgorny <radek@podgorny.cz>
2015-02-11 02:37:29 +0100

    faddnsc: fill host automatically

--- faddnsc.sh
+++ faddnsc.sh
@@ -15,9 +15,13 @@ schtasks /delete /tn "faddnsc" /f || true
 atxpkg install faddnsc
 
 cd c:/faddnsc
-sed -i 's/example.com/asterix.cz/g' faddnsc.ini
-nano -w faddnsc.ini
+sed -i "s/example.com/asterix.cz/g" faddnsc.ini
+mj=`ls c:/atx300 | grep mj`
+test $mj
+sed -i "s/;Host=.*/Host=$mj/g" faddnsc.ini
+#nano -w faddnsc.ini
 # TODO: add check for sensible setting of Host=
+cat faddnsc.ini
 ./install_service.bat
 cd -
 
