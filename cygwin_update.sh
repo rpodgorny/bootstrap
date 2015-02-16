d36c83405fe3629bb66ebd6550c1bf2d84ef19f8
Radek Podgorny <radek@podgorny.cz>
2015-02-16 12:43:44 +0100

    Update cygwin_update.sh

--- cygwin_update.sh
+++ cygwin_update.sh
@@ -7,4 +7,4 @@ wget -c http://cygwin.com/setup-x86.exe
 #http://ftp.fsn.hu/pub/cygwin
 
 chmod a+x setup-x86.exe
-./setup-x86.exe -s http://mirror.switch.ch/ftp/mirror/cygwin -l c:/tmp/ -q -g
+./setup-x86.exe -s http://mirror.switch.ch/ftp/mirror/cygwin -l c:/cygwin/tmp/ -q -g --no-desktop
