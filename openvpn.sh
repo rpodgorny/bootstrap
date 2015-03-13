0149cbb5f3c3c4740339b3dd81d38efcbeed49d7
Radek Podgorny <radek@podgorny.cz>
2015-03-13 15:59:34 +0100

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -4,6 +4,7 @@ set -x
 
 wget http://swupdate.openvpn.org/community/releases/openvpn-install-2.3.6-I002-x86_64.exe
 
+echo 'ted to asi vyblije hlasku o duveryhodnosti - potvrd to!'
 ./openvpn-install-2.3.6-I002-x86_64.exe /S
 
 sc config OpenVPNService start= auto
