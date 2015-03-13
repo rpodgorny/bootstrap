f8bcfbb7bbe7709c482ba04b89f66296d3de37eb
Radek Podgorny <radek@podgorny.cz>
2015-03-13 15:57:20 +0100

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -2,7 +2,11 @@
 set -e
 set -x
 
-schtasks /delete /tn "OpenVPN" /f
+wget http://swupdate.openvpn.org/community/releases/openvpn-install-2.3.6-I002-x86_64.exe
+
+./openvpn-install-2.3.6-I002-x86_64.exe /S
 
 sc config OpenVPNService start= auto
 net start OpenVPNService
+
+schtasks /delete /tn "OpenVPN" /f
