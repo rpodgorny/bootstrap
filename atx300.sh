f4312d3f6b87f7df295903e152f513b335334412
Radek Podgorny <radek@podgorny.cz>
2015-03-19 19:14:43 +0100

    Create atx300.sh

---
+++ atx300.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+/cygdrive/c/atxpkg/atxpkg install --yes \
+atx300-base \
+consist \
+control \
+loader \
+manager \
+memview \
+moistures \
+mpanel \
+param \
+place \
+router \
+signalsrecorder \
+simulator \
+visual \
+visual-data
