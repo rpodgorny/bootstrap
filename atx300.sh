f48a9d6ae50bac033ada6080f19114aa033bbf7f
Radek Podgorny <radek@podgorny.cz>
2015-03-19 19:25:02 +0100

    Update atx300.sh

--- atx300.sh
+++ atx300.sh
@@ -9,13 +9,14 @@ set -x
 
 rm -rf /cygdrive/c/atx300/mjxxx
 
+mkdir -p /cygdrive/c/atx300
+
 mj=`ls /cygdrive/c/atx300 | grep mj`
 if [ -z "$mj" ]; then
   echo
   echo "!!! ZADEJ JMENO BETONARNY (VCETNE MJ) !!! "
   read mj
 
-  mkdir -p /cygdrive/c/atx300
   touch /cygdrive/c/atx300/$mj
 fi
 
