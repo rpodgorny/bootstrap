6b795e6cf4a4e9e4354608d3d0879f47d3288038
Radek Podgorny <radek@podgorny.cz>
2015-03-19 19:32:09 +0100

    Update faddnsc.sh

--- faddnsc.sh
+++ faddnsc.sh
@@ -11,7 +11,8 @@ fi
 
 cd /cygdrive/c/faddnsc
 
-mj=`ls /cygdrive/c/atx300 | grep mj`
+# the cat is there only to eat the possible error when grep finds nothing
+mj=`ls /cygdrive/c/atx300 | grep mj | cat`
 if [ "$mj" ]; then
 	sed -i "s/example.com/asterix.cz/g" faddnsc.ini
 	sed -i "s/;Host=.*/Host=$mj/g" faddnsc.ini
