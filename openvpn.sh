ed233404e9f62ae78b89e505bfdeba2c20a3b141
Radek Podgorny <radek@podgorny.cz>
2015-03-19 18:34:12 +0100

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -11,6 +11,8 @@ echo
 echo '!!! TED TO ASI VYBLIJE HLASKU O DUVERYHODNOSTI - POTVRD TO! !!!'
 ./openvpn-install-2.3.6-I002-x86_64.exe /S
 
+rm openvpn-install-2.3.6-I002-x86_64.exe
+
 rm -f /cygdrive/c/Users/Public/Desktop/OpenVPN*lnk || true
 
 IFACE=`/cygdrive/c/Program\ Files/OpenVPN/bin/openvpn.exe --show-adapters | tail -n 1 | cut -f 2 -d"'"`
@@ -22,7 +24,12 @@ sc config OpenVPNService start= auto
 net start OpenVPNService
 
 echo
+echo '!!! CEKAM 10s !!!'
+sleep 10
+
+echo
 echo '!!! ZKONTROLUJ ADRESU !!!'
 netsh int ip show addresses asterix_openvpn
+netsh int ipv6 show addresses asterix_openvpn
 
 schtasks /delete /tn "OpenVPN" /f || true
