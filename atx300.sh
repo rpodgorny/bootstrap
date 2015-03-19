66794e97abb74413c6c1176b76599781c319ee1d
Radek Podgorny <radek@podgorny.cz>
2015-03-19 19:21:32 +0100

    Update atx300.sh

--- atx300.sh
+++ atx300.sh
@@ -2,6 +2,16 @@
 set -e
 set -x
 
+mj=`ls /cygdrive/c/atx300 | grep mj`
+if [ -z "$mj" ]; then
+  echo
+  echo "!!! ZADEJ JMENO BETONARNY (VCETNE MJ) !!! "
+  read mj
+
+  mkdir -p /cygdrive/c/atx300
+  touch /cygdrive/c/atx300/$mj
+fi
+
 /cygdrive/c/atxpkg/atxpkg install --yes \
 atx300-base \
 consist \
@@ -18,10 +28,3 @@ signalsrecorder \
 simulator \
 visual \
 visual-data
-
-echo
-echo "!!! ZADEJ JMENO BETONARNY (VCETNE MJ) !!! "
-read mj
-
-mkdir -p /cygdrive/c/atx300
-touch /cygdrive/c/atx300/$mj
