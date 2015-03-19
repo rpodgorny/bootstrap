3e2f521a1aabf5aca243919251c5a10d5bb501fa
Radek Podgorny <radek@podgorny.cz>
2015-03-19 17:56:29 +0100

    Update all.sh

--- all.sh
+++ all.sh
@@ -9,3 +9,7 @@ chmod a+x cygwin_init.sh
 wget http://rawgit.com/rpodgorny/bootstrap/master/4_incoming_setup.sh
 chmod a+x 4_incoming_setup.sh
 ./4_incoming_setup.sh
+
+wget http://rawgit.com/rpodgorny/bootstrap/master/atxpkg.sh
+chmod a+x atxpkg.sh
+./atxpkg.sh
