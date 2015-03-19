c84722856d5691fb9e625cffcb23b204fcd035a7
Radek Podgorny <radek@podgorny.cz>
2015-03-19 12:47:58 +0100

    Update cygwin_init.sh

--- cygwin_init.sh
+++ cygwin_init.sh
@@ -6,7 +6,7 @@ if [ ! -f /etc/sshd_config ]; then
 	ssh-host-config --yes --pwd atx
 fi
 
-if [ grep "^AllowUsers remoteadmin remotebackup" /etc/sshd_config ]; then
+if [ grep "AllowUsers remoteadmin remotebackup" /etc/sshd_config ]; then
 	sed -e "s/^AllowUsers.*/AllowUsers remoteadmin remotebackup/g" -i /etc/sshd_config
 fi
 
