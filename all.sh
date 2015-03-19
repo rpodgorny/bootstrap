46302107a87c6909afe588c59977ef339659293d
Radek Podgorny <radek@podgorny.cz>
2015-03-19 18:26:58 +0100

    Update all.sh

--- all.sh
+++ all.sh
@@ -5,15 +5,19 @@ set -x
 wget http://rawgit.com/rpodgorny/bootstrap/master/cygwin_init.sh
 chmod a+x cygwin_init.sh
 ./cygwin_init.sh
+rm cygwin_init.sh
 
 wget http://rawgit.com/rpodgorny/bootstrap/master/4_incoming_setup.sh
 chmod a+x 4_incoming_setup.sh
 ./4_incoming_setup.sh
+rm 4_incoming_setup.sh
 
 wget http://rawgit.com/rpodgorny/bootstrap/master/atxpkg.sh
 chmod a+x atxpkg.sh
 ./atxpkg.sh
+rm atxpkg.sh
 
 wget http://rawgit.com/rpodgorny/bootstrap/master/openvpn.sh
 chmod a+x openvpn.sh
 ./openvpn.sh
+rm openvpn.sh
