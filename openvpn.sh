af447d7ac8359b9c6ae4f9312af7b0a29dec9f41
Radek Podgorny <radek@podgorny.cz>
2015-03-19 18:04:28 +0100

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -5,12 +5,14 @@ set -x
 # vzdy aktualni verze na (stahuj verzi pro xp): https://openvpn.net/index.php/open-source/downloads.html
 wget http://swupdate.openvpn.org/community/releases/openvpn-install-2.3.6-I002-x86_64.exe
 
-echo 'ted to asi vyblije hlasku o duveryhodnosti - potvrd to!'
+echo
+echo '!!! TED TO ASI VYBLIJE HLASKU O DUVERYHODNOSTI - POTVRD TO! !!!'
 ./openvpn-install-2.3.6-I002-x86_64.exe /S
 
 rm -f /cygdrive/c/Users/Public/Desktop/OpenVPN*lnk || true
 
-echo 'prejmenuj adapter na asterix_openvpn'
+echo
+echo '!!! PREJMENUJ ADAPTER NA asterix_openvpn A PAK STISKNI ENTER !!!'
 read
 
 /cygdrive/c/atxpkg/atxpkg install openvpn-asterix --yes
@@ -18,7 +20,8 @@ read
 sc config OpenVPNService start= auto
 net start OpenVPNService
 
-echo 'zkontroluj adresu'
+echo
+echo '!!! ZKONTROLUJ ADRESU !!!'
 netsh int ip show addresses
 
 schtasks /delete /tn "OpenVPN" /f || true
