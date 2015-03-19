81c066027c1fd7745deaff9620aa6e5ef0d5f720
Radek Podgorny <radek@podgorny.cz>
2015-03-19 17:44:32 +0100

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,6 +2,10 @@
 set -e
 set -x
 
+wget http://rawgit.com/rpodgorny/bootstrap/master/cygwin_init.sh
+chmod a+x cygwin_init.sh
+./cygwin_init.sh
+
 wget http://rawgit.com/rpodgorny/bootstrap/master/4_incoming_setup.sh
 chmod a+x 4_incoming_setup.sh
 ./4_incoming_setup.sh
