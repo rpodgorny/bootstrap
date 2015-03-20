31f9e729796b1e6fa90aa69f826928c76d50189c
Radek Podgorny <radek@podgorny.cz>
2015-03-20 15:09:42 +0100

    Update atx300.sh

--- atx300.sh
+++ atx300.sh
@@ -2,14 +2,14 @@
 set -e
 set -x
 
-#if [ -d /cygdrive/c/atx300 ]; then
-#  echo 'atx300 already installed?'
-#  exit 1
-#fi
-
-rm -rf /cygdrive/c/atx300/mjxxx
+if [ -d /cygdrive/c/atx300 ]; then
+  echo 'atx300 already installed?'
+  exit 1
+fi
 
 mkdir -p /cygdrive/c/atx300
+
+net share atx300=c:\\atx300
 
 # the cat is there only to eat the possible error when grep finds nothing
 mj=`ls /cygdrive/c/atx300 | grep mj | cat`
