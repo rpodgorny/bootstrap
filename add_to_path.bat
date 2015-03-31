9afdff1ad81dcbd51c1fdd3a20d3b50eace82ccd
Radek Podgorny <radek@podgorny.cz>
2015-03-31 16:23:49 +0200

    Update add_to_path.bat

--- add_to_path.bat
+++ add_to_path.bat
@@ -1,17 +1,9 @@
 set p=c:\cygwin\bin
 
 Echo.%PATH% | findstr /C:"%p%">nul && (
-	echo %p% already in path
+  echo %p% already in path
 ) || (
-	echo adding to %p% to path
-	setx PATH "%PATH%;%p%" -m
-)
-
-set p=c:\cygwin\usr\sbin
-
-Echo.%PATH% | findstr /C:"%p%">nul && (
-	echo %p% already in path
-) || (
-	echo adding to %p% to path
-	setx PATH "%PATH%;%p%" -m
+  echo adding to %p% to path
+  setx PATH "%PATH%;%p%" -m
+  set "PATH=%PATH%;%p%"
 )
