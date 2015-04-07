6b14e27aeb57ac24270bffe21c0ca5fd95762ff9
Radek Podgorny <radek@podgorny.cz>
2015-04-07 16:38:53 +0200

    Create 0.bat

---
+++ 0.bat
@@ -0,0 +1,8 @@
+powershell "(new-object net.webclient).downloadfile('http://rawgit.com/rpodgorny/bootstrap/master/cygwin.bat', 'cygwin.bat')"
+call cygwin.bat
+del cygwin.bat
+
+powershell "(new-object net.webclient).downloadfile('http://rawgit.com/rpodgorny/bootstrap/master/all.sh', 'all.sh')"
+chmod a+x all.sh
+bash all.sh
+del all.sh
