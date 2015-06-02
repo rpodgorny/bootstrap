ff738e6f1d971582065036c06fa4da996c1aa64d
Radek Podgorny <radek@podgorny.cz>
2015-06-02 17:32:35 +0200

    Update other.sh

--- other.sh
+++ other.sh
@@ -2,9 +2,14 @@
 set -e
 set -x
 
+# disable non-native ipv6 shit
 netsh int ipv6 isatap set state disabled
 netsh int ipv6 6to4 set state disabled
 netsh interface teredo set state disabled
+
+# disable privacy extensions
+netsh interface ipv6 set privacy state=disabled store=active
+netsh interface ipv6 set privacy state=disabled store=persistent
 
 # enable aaaa lookups even for non-native ipv6 connectivity
 regtool -d add /HKLM/SYSTEM/CurrentControlSet/services/Dnscache/Parameters AddrConfigControl
