1c98f2c802a62fe1892b4ff78d05f1dc697101bd
Radek Podgorny <radek@podgorny.cz>
2015-06-25 14:04:59 +0200

    Update other.sh

--- other.sh
+++ other.sh
@@ -3,8 +3,8 @@ set -e
 set -x
 
 # disable non-native ipv6 shit
-netsh int ipv6 isatap set state disabled
-netsh int ipv6 6to4 set state disabled
+netsh interface ipv6 isatap set state disabled
+netsh interface ipv6 6to4 set state disabled
 netsh interface teredo set state disabled || true
 
 # disable privacy extensions
