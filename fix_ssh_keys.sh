ccecc315d31f0dff9a031cd3a1ca8c0bb8392e5d
Radek Podgorny <radek@podgorny.cz>
2015-08-06 13:37:07 +0200

    fix_ssh_keys: make it non-interactive

--- fix_ssh_keys.sh
+++ fix_ssh_keys.sh
@@ -8,12 +8,17 @@ if [ -f /home/admin/.ssh/id_rsa.pub ]; then
   exit 0
 fi
 
+test -f /home/admin/.ssh/id_dsa.pub
+test -f /home/admin/.ssh/id_dsa
+
 ssh-keygen -t rsa -f /home/admin/.ssh/id_rsa -P ""
 
-echo
-echo '!!! OVER IDENTITU (A ZADEJ yes) A VYPLN HESLO !!!'
+#echo
+#echo '!!! OVER IDENTITU (A ZADEJ yes) A VYPLN HESLO !!!'
 
-ssh-copy-id -i /home/admin/.ssh/id_rsa incoming@incoming.asterix.cz
+#ssh-copy-id -i /home/admin/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null incoming@incoming.asterix.cz
+
+cat /home/admin/.ssh/id_rsa.pub | ssh -i /home/admin/.ssh/id_dsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null incoming@incoming.asterix.cz 'cat >> .ssh/authorized_keys'
 
 rm -f /home/admin/.ssh/id_dsa.pub
 rm -f /home/admin/.ssh/id_dsa
@@ -21,5 +26,5 @@ rm -f /home/admin/.ssh/id_dsa
 echo
 echo '!!! TED BY TO MELO VYPSAT INFO O SYSTEMU NA incoming.asterix.cz !!!'
 
-ssh -i /home/admin/.ssh/id_rsa incoming@incoming.asterix.cz 'uname -a'
+ssh -i /home/admin/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null incoming@incoming.asterix.cz 'uname -a'
 
