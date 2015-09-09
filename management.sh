080f87ebd46b8852994b9b83fa78f509ae6c0069
Radek Podgorny <radek@podgorny.cz>
2015-09-09 17:19:54 +0200

    management: exit early if management already installed

--- management.sh
+++ management.sh
@@ -2,6 +2,11 @@
 set -e
 set -x
 
+if [ $(/cygdrive/c/atxpkg/atxpkg list_installed | grep management) ]; then
+  echo 'management already installed?'
+  exit 0
+fi
+
 /cygdrive/c/atxpkg/atxpkg install management --yes --force
 
 cd /cygdrive/c/atx300/management
