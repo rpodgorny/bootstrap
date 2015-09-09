163562e5f7364d2a28b41980798febad96baa53a
Radek Podgorny <radek@podgorny.cz>
2015-09-10 01:19:04 +0200

    add other services to service_restart

--- service_restart.sh
+++ service_restart.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -e -x
 
+sc failure "4to6server" actions= restart/600000/restart/600000/restart/600000 reset= 86400
+sc failure "atxmonc" actions= restart/600000/restart/600000/restart/600000 reset= 86400
+sc failure "faddnsc" actions= restart/600000/restart/600000/restart/600000 reset= 86400
 sc failure "sshd" actions= restart/600000/restart/600000/restart/600000 reset= 86400
+sc failure "tvnserver" actions= restart/600000/restart/600000/restart/600000 reset= 86400
+sc failure "OpenVPNService" actions= restart/600000/restart/600000/restart/600000 reset= 86400
