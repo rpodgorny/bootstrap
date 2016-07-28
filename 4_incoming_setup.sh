e25d49f26505e40435b5c223f10b430a63b91b91
Radek Podgorny <radek@podgorny.cz>
2016-07-28 23:42:49 +0200

    incoming setup: specify identity file

--- 4_incoming_setup.sh
+++ 4_incoming_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-set -e
-set -x
+set -e -x
 
 if [ -f /home/admin/.ssh/id_rsa.pub ]; then
   echo 'incoming already configured?'
@@ -11,8 +10,8 @@ ssh-keygen -t rsa -f /home/admin/.ssh/id_rsa -P ""
 
 echo
 echo '!!! OVER IDENTITU (A ZADEJ yes) A VYPLN HESLO !!!'
-ssh-copy-id incoming@incoming.asterix.cz
+ssh-copy-id -i /home/admin/.ssh/id_rsa incoming@incoming.asterix.cz
 
 echo
 echo '!!! TED BY TO MELO VYPSAT INFO O SYSTEMU NA incoming.asterix.cz !!!'
-ssh incoming@incoming.asterix.cz 'uname -a'
+ssh -i /home/admin/.ssh/id_rsa incoming@incoming.asterix.cz 'uname -a'
