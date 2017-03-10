66668f88b44ac46eca767d0bf2081cd0fdb91e5e
Radek Podgorny <radek@podgorny.cz>
2017-03-10 02:46:16 +0100

    use trap

--- install_kb2999226.sh
+++ install_kb2999226.sh
@@ -3,6 +3,10 @@ set -e -x
 
 # get the shit from https://www.microsoft.com/en-us/download/confirmation.aspx?id=49093
 
+function finish {
+  rm Windows6.1-KB2999226-x64.msu
+}
+trap finish EXIT
+
 wget http://admiral.podgorny.cz/~radek/Windows6.1-KB2999226-x64.msu
 wusa.exe Windows6.1-KB2999226-x64.msu /quiet
-rm Windows6.1-KB2999226-x64.msu
