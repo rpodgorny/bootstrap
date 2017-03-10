0a6ce8176616c78a1590e93cf85cd2d12bff7815
Radek Podgorny <radek@podgorny.cz>
2017-03-10 02:29:46 +0100

    add comment

--- install_kb2999226.sh
+++ install_kb2999226.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e -x
 
+# get the shit from https://www.microsoft.com/en-us/download/confirmation.aspx?id=49093
+
 wget http://admiral.podgorny.cz/~radek/Windows6.1-KB2999226-x64.msu
 wusa.exe Windows6.1-KB2999226-x64.msu /quiet
 rm Windows6.1-KB2999226-x64.msu
