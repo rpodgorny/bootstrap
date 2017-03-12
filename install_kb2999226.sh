d9314bf36dfe12a7817327ff1ae362c134a1f33f
Radek Podgorny <radek@podgorny.cz>
2017-03-12 20:48:27 +0100

    install_kb2999226.sh: add support for 32bit windows

--- install_kb2999226.sh
+++ install_kb2999226.sh
@@ -2,11 +2,15 @@
 set -e -x
 
 # get the shit from https://www.microsoft.com/en-us/download/confirmation.aspx?id=49093
+# 32bit: https://www.microsoft.com/en-us/download/confirmation.aspx?id=49077
+
+#FN=Windows6.1-KB2999226-x64.msu
+FN=Windows6.1-KB2999226-x86.msu
 
 function finish {
-  rm Windows6.1-KB2999226-x64.msu
+  rm $FN
 }
 trap finish EXIT
 
-wget http://admiral.podgorny.cz/~radek/Windows6.1-KB2999226-x64.msu
-wusa.exe Windows6.1-KB2999226-x64.msu /quiet
+wget http://admiral.podgorny.cz/~radek/$FN
+wusa.exe $FN /quiet
