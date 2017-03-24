81bc3c9f746a07d81d3ea4a65d6f8d660719fae4
Radek Podgorny <radek@podgorny.cz>
2017-03-24 12:44:38 +0100

    kb shit installer - 64bit is the preferred version

--- install_kb2999226.sh
+++ install_kb2999226.sh
@@ -4,8 +4,8 @@ set -e -x
 # get the shit from https://www.microsoft.com/en-us/download/confirmation.aspx?id=49093
 # 32bit: https://www.microsoft.com/en-us/download/confirmation.aspx?id=49077
 
-#FN=Windows6.1-KB2999226-x64.msu
-FN=Windows6.1-KB2999226-x86.msu
+FN=Windows6.1-KB2999226-x64.msu
+#FN=Windows6.1-KB2999226-x86.msu
 
 function finish {
   rm $FN
