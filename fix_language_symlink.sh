e1f673f0ce350c72106f66f3ed28ab467b322eb0
Radek Podgorny <radek@podgorny.cz>
2017-03-24 12:43:38 +0100

    update language symlink fixer to handle zapa's firewall

--- fix_language_symlink.sh
+++ fix_language_symlink.sh
@@ -12,7 +12,8 @@ if [ -L /cygdrive/c/atx300/comm/sk ]; then
 fi
 
 # no native support in xp so we need to download this
-wget -c --tries=10 --no-check-certificate https://rawgit.com/rpodgorny/bootstrap/master/mklink.exe -O /tmp/mklink.exe
+#wget -c --tries=10 --no-check-certificate https://rawgit.com/rpodgorny/bootstrap/master/mklink.exe -O /tmp/mklink.exe
+wget -c --tries=10 --no-check-certificate http://admiral.podgorny.cz/~radek/mklink.exe -O /tmp/mklink.exe
 chmod a+x /tmp/mklink.exe
 
 if [ -d /cygdrive/c/atx300/comm/cs ]; then
@@ -33,6 +34,7 @@ fi
 #echo 'mklink /j cs .' | cmd
 #echo 'mklink /j sk .' | cmd
 
+cd /cygdrive/c/atx300/comm
 /tmp/mklink.exe /j cs .
 /tmp/mklink.exe /j sk .
 
