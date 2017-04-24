194cfb12ab2c88ebeae59e715b98cbb244f05dd4
Radek Podgorny <radek@podgorny.cz>
2017-04-25 01:24:27 +0200

    atxmonc: updated conf

--- atxmonc.sh
+++ atxmonc.sh
@@ -2,8 +2,8 @@
 set -e -x
 
 if [ -d /cygdrive/c/atxmonc ]; then
-	echo 'atxmonc already installed?'
-	exit 0
+  echo 'atxmonc already installed?'
+  exit 0
 fi
 
 /cygdrive/c/atxpkg/atxpkg install atxmonc --yes
@@ -13,18 +13,17 @@ cd /cygdrive/c/atxmonc
 # the cat is there only to eat the possible error when grep finds nothing
 mj=`ls /cygdrive/c/atx300 | grep mj | cat`
 if [ "$mj" ]; then
-	sed -i "s/example.com/asterix.cz/g" atxmonc.ini
-	sed -i "s/;Host=.*/Host=$mj/g" atxmonc.ini
+  sed -i "s/example.com/asterix.cz/g" atxmonc.conf
+  sed -i "s/;Host=.*/Host=$mj/g" atxmonc.conf
 else
-	echo
-	echo "!!! NENASEL JSEM CISLO MJ, SPUSTIM EDITOR PRO RUCNI EDITACI. POKRACUJTE ENTEREM !!!"
-	read
-
-	nano -w atxmonc.ini
+  echo
+  echo "!!! NENASEL JSEM CISLO MJ, SPUSTIM EDITOR PRO RUCNI EDITACI. POKRACUJTE ENTEREM !!!"
+  read
+  nano -w atxmonc.conf
 fi
 
 # TODO: add check for sensible setting of Host=
-cat atxmonc.ini
+cat atxmonc.conf
 ./install_service.bat
 cd -
 
