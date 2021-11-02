a44d137685a097eed5a9a29a40316c301eebd948
Radek Podgorny <radek@podgorny.cz>
2021-11-02 12:02:25 +0100

    zapa_time tweaks

--- zapa_time.sh
+++ zapa_time.sh
@@ -2,7 +2,10 @@
 set -e -x
 net start w32time || true
 w32tm /config /manualpeerlist:"10.32.32.44" /syncfromflags:manual /reliable:yes /update
+# give it some time to stop the service (why does it even stop it?)
+sleep 5
 # the above command sometimes stops the service - start it again just to be sure
 net start w32time || true
+# also, force the resync just to be sure (sometimes this actaully updates the time)
 w32tm /resync
 date
