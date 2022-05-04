4ee360808d4152f8b75c465af82f821aa1969b92
Radek Podgorny <radek@podgorny.cz>
2022-05-04 14:37:37 +0200

    add port to 4_incoming bootstrap script

--- 4_incoming_setup.sh
+++ 4_incoming_setup.sh
@@ -10,8 +10,8 @@ ssh-keygen -t rsa -f /home/admin/.ssh/id_rsa -P ""
 
 echo
 echo '!!! OVER IDENTITU (A ZADEJ yes) A VYPLN HESLO !!!'
-ssh-copy-id -i /home/admin/.ssh/id_rsa incoming@incoming.asterix.cz
+ssh-copy-id -i /home/admin/.ssh/id_rsa -p 2255 incoming@incoming.asterix.cz
 
 echo
 echo '!!! TED BY TO MELO VYPSAT INFO O SYSTEMU NA incoming.asterix.cz !!!'
-ssh -i /home/admin/.ssh/id_rsa incoming@incoming.asterix.cz 'uname -a'
+ssh -i /home/admin/.ssh/id_rsa -p 2255 incoming@incoming.asterix.cz 'uname -a'
