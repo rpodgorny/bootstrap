be114ded254a40c12ae534faf398dbff35a6f941
Radek Podgorny <radek@podgorny.cz>
2023-01-18 16:44:13 +0100

    experimental changes and updates to openvpn

--- openvpn.sh
+++ openvpn.sh
@@ -9,20 +9,24 @@ fi
 # vzdy aktualni verze na: https://openvpn.net/index.php/open-source/downloads.html
 
 # TODO: mozna pozor na jiny driver pro win8
-EXE="openvpn-install-2.4.9-I601-Win10.exe"
-URL="https://swupdate.openvpn.org/community/releases/${EXE}"
+#EXE="openvpn-install-2.4.9-I601-Win10.exe"
+#URL="https://swupdate.openvpn.org/community/releases/${EXE}"
+MSI="OpenVPN-2.5.8-I604-x86.msi"
+URL="https://swupdate.openvpn.org/community/releases/${MSI}"
 
 wget -c --tries=10 ${URL}
 
-chmod a+x $EXE
+#chmod a+x $EXE
 
 echo
 echo '!!! TED TO ASI VYBLIJE HLASKU O DUVERYHODNOSTI - POTVRD TO! !!!'
-./$EXE /S
+#./$EXE /S
+msiexec ${MSI} /SELECT_SERVICE=1
 
-rm $EXE
+#rm $EXE
+rm ${MSI}
 
-rm -f /cygdrive/c/Users/Public/Desktop/OpenVPN*lnk || true
+#rm -f /cygdrive/c/Users/Public/Desktop/OpenVPN*lnk || true
 
 IFACE=`/cygdrive/c/Program\ Files/OpenVPN/bin/openvpn.exe --show-adapters | tail -n 1 | cut -f 2 -d"'"`
 netsh interface set interface "$IFACE" newname = "asterix_openvpn"
