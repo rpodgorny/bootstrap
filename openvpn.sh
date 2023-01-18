4c4eee1bf647e6be5658b81ad60f32a349d65a9e
Radek Podgorny <radek@podgorny.cz>
2023-01-18 17:15:23 +0100

    openvpn script update

--- openvpn.sh
+++ openvpn.sh
@@ -8,25 +8,17 @@ fi
 
 # vzdy aktualni verze na: https://openvpn.net/index.php/open-source/downloads.html
 
-# TODO: mozna pozor na jiny driver pro win8
-#EXE="openvpn-install-2.4.9-I601-Win10.exe"
-#URL="https://swupdate.openvpn.org/community/releases/${EXE}"
-MSI="OpenVPN-2.5.8-I604-x86.msi"
+MSI="OpenVPN-2.5.8-I604-amd64.msi"
 URL="https://swupdate.openvpn.org/community/releases/${MSI}"
 
 wget -c --tries=10 ${URL}
 
-#chmod a+x $EXE
+# https://community.openvpn.net/openvpn/wiki/OpenVPN2.5_Windows_MSI_Unattended_Install
+msiexec /i ${MSI} ADDLOCAL=OpenVPN.GUI,OpenVPN.GUI.OnLogon,OpenVPN.Service,OpenVPN,Drivers,Drivers.TAPWindows6,Drivers.Wintun /passive
 
-echo
-echo '!!! TED TO ASI VYBLIJE HLASKU O DUVERYHODNOSTI - POTVRD TO! !!!'
-#./$EXE /S
-msiexec ${MSI} /SELECT_SERVICE=1
-
-#rm $EXE
 rm ${MSI}
 
-#rm -f /cygdrive/c/Users/Public/Desktop/OpenVPN*lnk || true
+rm -f /cygdrive/c/Users/Public/Desktop/OpenVPN*lnk || true
 
 IFACE=`/cygdrive/c/Program\ Files/OpenVPN/bin/openvpn.exe --show-adapters | tail -n 1 | cut -f 2 -d"'"`
 netsh interface set interface "$IFACE" newname = "asterix_openvpn"
