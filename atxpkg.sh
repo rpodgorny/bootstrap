d3fc784e765e698c221f2a2b27fd044cee2c083f
Radek Podgorny <radek@podgorny.cz>
2023-03-31 16:39:54 +0200

    atxpkg

--- atxpkg.sh
+++ atxpkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e -x
 
-#if [ -d /cygdrive/c/atxpkg ]; then
+#if [ -d c:/atxpkg ]; then
 #  echo 'atxpkg already installed?'
 #  exit 0
 #fi
@@ -14,8 +14,8 @@ fn=$pkg-$ver.atxpkg.zip
 #pkg=atxpkg.dev
 #fn=atxpkg.dev-20220203181342-1.atxpkg.zip
 
-mkdir -p /cygdrive/c/tmp
-cd /cygdrive/c/tmp
+mkdir -p c:/tmp
+cd c:/tmp
 
 rm -rf atxpkg.tmp
 mkdir atxpkg.tmp
@@ -27,31 +27,32 @@ wget --no-check-certificate $urlbase/$fn
 #wget https://atxpkg.asterix.cz/7za.exe
 wget --no-check-certificate https://atxpkg.asterix.cz/7za.exe
 
-cp /cygdrive/c/atxpkg/installed.json ./ || true
+cp c:/atxpkg/installed.json ./ || true
 chmod a+r installed.json || true
 
 chmod a+x 7za.exe
 ./7za.exe x $fn
-rm -rf /cygdrive/c/atxpkg/* || true
-cp -rv atxpkg c:\\
+rm -rf c:/atxpkg/* || true
+cp -rv atxpkg c:/
 #rm -rf atxpkg
-mkdir -p /cygdrive/c/atxpkg/cache
-cp $fn /cygdrive/c/atxpkg/cache/
+mkdir -p c:/atxpkg/cache
+cp $fn c:/atxpkg/cache/
 #rm $fn
 #rm 7za.exe
 
-cp installed.json /cygdrive/c/atxpkg/ || true
+cp installed.json c:/atxpkg/ || true
 #rm installed.json || true
 
 cd -
 rm -rf atxpkg.tmp
 
-/cygdrive/c/atxpkg/atxpkg install $pkg-$ver --yes --force
+c:/atxpkg/atxpkg install $pkg-$ver --yes --force
 
-cd /cygdrive/c/atxpkg
+cd c:/atxpkg
 ./add_to_path.bat
 cd -
 
+# so that the new path is applied
 net stop cygsshd || true
 net start cygsshd || true
 
