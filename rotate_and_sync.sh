51852e297a4a8701c81632c2100807f775f31197
Radek Podgorny <radek@podgorny.cz>
2023-04-04 18:07:17 +0200

    rotate_and_sync: msys2 updates

--- rotate_and_sync.sh
+++ rotate_and_sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e -x
-touch /cygdrive/c/atx300/signalsrecorder/rotate
-touch /cygdrive/c/atx300/management/sync
-tail -n 0 -f /cygdrive/c/atx300/log/management.log | grep -m 1 "sync success"
+touch c:/atx300/signalsrecorder/rotate
+touch c:/atx300/management/sync
+tail -n 0 -f c:/atx300/log/management.log | grep -m 1 "sync success"
