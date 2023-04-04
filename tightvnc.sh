d6c8b56ec1b098a469d47ec8a858c6c31713cdc0
Radek Podgorny <radek@podgorny.cz>
2023-04-04 19:56:24 +0200

    add tightvnc

---
+++ tightvnc.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e -x
+
+# TODO: find out the actual name of directory
+if [ -d c:/Program\ Files/TightVNC ]; then
+  echo 'tightvnc already installed?'
+  exit 0
+fi
+
+# vzdy aktualni verze na: https://www.tightvnc.com/download.html
+
+https://www.tightvnc.com/download/2.8.78/
+
+VER="2.8.78"
+MSI="tightvnc-${VER}-gpl-setup-64bit.msi"
+URL="https://www.tightvnc.com/download/${VER}/${MSI}"
+
+wget -c --tries=10 ${URL}
+
+# https://www.tightvnc.com/doc/win/TightVNC_2.7_for_Windows_Installing_from_MSI_Packages.pdf
+if [ -d c:/cygwin ]; then
+  msiexec /i ${MSI} /quiet /norestart \
+    ADDLOCAL="Server,Viewer" \
+    SET_ACCEPTHTTPCONNECTIONS=1 VALUE_OF_ACCEPTHTTPCONNECTIONS=0 \
+    SET_ACCEPTRFBCONNECTIONS=1 VALUE_OF_ACCEPTRFBCONNECTIONS=0 \
+    SET_ALLOWLOOPBACK=1 VALUE_OF_ALLOWLOOPBACK=1 \
+    SET_USEVNCAUTHENTICATION=1 \
+    VALUE_OF_USEVNCAUTHENTICATION=1 \
+    SET_PASSWORD=1 \
+    VALUE_OF_PASSWORD=atx438c \
+    SET_VIEWONLYPASSWORD=1 \
+    VALUE_OF_VIEWONLYPASSWORD=atx438w \
+else
+  # msys2
+  msiexec //i ${MSI} //quiet //norestart \
+    ADDLOCAL="Server,Viewer" \
+    SET_ACCEPTHTTPCONNECTIONS=1 VALUE_OF_ACCEPTHTTPCONNECTIONS=0 \
+    SET_ACCEPTRFBCONNECTIONS=1 VALUE_OF_ACCEPTRFBCONNECTIONS=0 \
+    SET_ALLOWLOOPBACK=1 VALUE_OF_ALLOWLOOPBACK=1 \
+    SET_USEVNCAUTHENTICATION=1 \
+    VALUE_OF_USEVNCAUTHENTICATION=1 \
+    SET_PASSWORD=1 \
+    VALUE_OF_PASSWORD=atx438c \
+    SET_VIEWONLYPASSWORD=1 \
+    VALUE_OF_VIEWONLYPASSWORD=atx438w \
+fi
+
+rm ${MSI}
+
+#rm -f c:/Users/Public/Desktop/OpenVPN*lnk || true
+
+sc config tvncserver start= auto
+sc failure "tvnserver" actions= restart/600000/restart/600000/restart/600000 reset= 86400
+
+net stop tvnserver || true
+net start tvnserver
