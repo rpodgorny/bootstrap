1ec7bae52dd4b5cc689776822e06f135b10a5dd6
Radek Podgorny <radek@podgorny.cz>
2023-04-05 21:08:48 +0200

    openvpn: typo fix

--- openvpn.sh
+++ openvpn.sh
@@ -7,6 +7,7 @@ if [ -d c:/Program\ Files/OpenVPN ]; then
   if [ "${YESNO}" = "y" ]; then
     PROD=`wmic product get name | grep OpenVPN`
     wmic product where name="'${PROD}'" call uninstall
+  fi
   exit 0
 fi
 
